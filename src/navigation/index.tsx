b50223a4d82e532d16fcd2022a9ccbfce1d5ea92
mihrilp <mihrilp@gmail.com>
2023-08-21 16:07:38 +0300

    feat: Implement Tab Bar navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,14 +1,29 @@
 import Categories from "@/screens/Categories";
 import Home from "@/screens/Home";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator<BottomBarScreens>();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Categories" component={Categories} />
+    </Tab.Navigator>
+  );
+};
 
 export function RootStackNavigator() {
   return (
-    <RootStack.Navigator initialRouteName="Home">
-      <RootStack.Screen name="Home" component={Home} />
-      <RootStack.Screen name="Categories" component={Categories} />
+    <RootStack.Navigator
+      initialRouteName="Main"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <RootStack.Screen name="Main" component={TabNavigator} />
     </RootStack.Navigator>
   );
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,12 @@
 export {};
 
 declare global {
+  //navigation
   type RootStackParamList = {
+    Main: NavigatorScreenParams<BottomBarScreens>;
+  };
+
+  type BottomBarScreens = {
     Home: undefined;
     Categories: undefined;
   };
